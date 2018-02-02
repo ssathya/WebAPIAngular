e7954a1a4fdc819be22ed60685735d0be0b506e9
Sridhar Sathya <sridhar_0314@live.com>
2018-02-02 16:36:52 -0500

    Starting all over Created service AuthService, added bootstrap to index.html

--- ApiClient/src/app/app.module.ts
+++ ApiClient/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
